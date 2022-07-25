578093ca7a547dd836de4801a03478d5b50a82bd
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-07-26 01:40:02 +0600

    added cover

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
+  env: {
+    GOOGLE_ANALYTICS_ID: "G-8BHR6064F5",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,38 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import "../styles/reset.css";
 import "../styles/globals.scss";
-import { ParallaxProvider } from "react-scroll-parallax";
+import Script from "next/script";
+import * as ga from "../lib/google-analitycs";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouterChange = (url) => {
+      ga.pageview(url);
+    };
+
+    router.events.on("routerChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routerChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
   return (
-    <ParallaxProvider>
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-8BHR6064F5"
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-analytics-script" strategy="afterInteractive">
+        {` window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-8BHR6064F5');`}
+      </Script>
       <Component {...pageProps} />
-    </ParallaxProvider>
+    </>
   );
 }
 
